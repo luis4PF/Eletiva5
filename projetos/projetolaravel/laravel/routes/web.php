1b39e3e5ccca3d1f85bb8ed73f436a4b431d5b7d
luis4PF <luishtardem@gmail.com>
2022-08-23 20:42:27 -0300

    commit

---
+++ projetos/projetolaravel/laravel/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('usuarios')->group(function(){
+    Route::get('',function(){
+        return 'usuario';
+    });
+    Route::get('/{id}', function($id) {
+        return 'Mostrar detalhes usuario' .$id;
+    });
+    Route::get('/{id}/tags', function($id){
+        return 'Tags do usuario' .$id;
+    });
+});
+
+Route::get('/users/{paramA}/{paramB}', function($paramA, $paramB){
+    return 'Parametros: ' .$paramA . ' - ' .$paramB;
+});
+
+Route::get('/users/id', function($id = null){
+    return 'Id Usuario: ' .$id;
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
